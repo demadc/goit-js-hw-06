f310507967058f18e805160cb9f5097216e4dfbf
Demian <demyan.korotchenko@gmail.com>
2023-06-12 14:05:31 +0200

    1234

--- js/task-09.js
+++ js/task-09.js
@@ -21,13 +21,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const textColor = document.querySelector('.color');
-const changeColorBtn = document.querySelector('.change-color');
+const textColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
 const body = document.body;
 
 const changeColor = () => {
-   body.style.backgroundColor = getRandomHexColor();
-   textColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = getRandomHexColor();
 };
 
-changeColorBtn.addEventListener('click', changeColor);
+changeColorBtn.addEventListener("click", changeColor);
